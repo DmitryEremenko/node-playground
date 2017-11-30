b1afcb854ce1c80102ac7b26278e5b17f2c502ac
Dmitry Yeremenko <Dmitry.Yeremenko@dataart.com>
2017-11-30 13:42:55 +0200

    Init commet

---
+++ playground/mongodb-find.js
@@ -0,0 +1,27 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect');
+  }
+  console.log('Connected to MongoDb server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5a1c2d0f1dc969170455971f')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({
+    name: 'Dmitry'
+  }).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.close();
+});
